79544315d40604f72964e1e6263d0a025f6f735c
Paul Gomez <pauldro952@outlook.com>
2018-10-15 11:52:59 -0500

    Update for directory name changes

---
+++ src/OrderDisplays/QuoteDisplay.interface.php
@@ -0,0 +1,16 @@
+<?php 
+	namespace Dplus\Dpluso\OrderDisplays;
+	
+	/**
+	 * Functions QuoteDisplay classes have to implement
+	 */
+    interface QuoteDisplayInterface {
+		/**
+		 * Returns Quote Details
+		 * @param  Order  $quote Quote $quote
+		 * @param  bool   $debug Whether or not to execute Query
+		 * @return array         array of QuoteDetail | SQL Query
+		 * @uses
+		 */
+        public function get_quotedetails(Order $quote, $debug = false);
+    }
