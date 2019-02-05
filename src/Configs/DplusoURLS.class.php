1435d1b9735b780cd82ec0d1ab7d54356a622472
Paul Gomez <pauldro952@outlook.com>
2019-02-05 15:43:26 -0600

    updates to dpluso urls config

--- src/Configs/DplusoURLS.class.php
+++ src/Configs/DplusoURLS.class.php
@@ -1,13 +1,80 @@
 <?php
 	namespace Dplus\Dpluso\Configs;
 	
-	use ProcessWire\WireData;
+	use Dplus\Base\ThrowErrorTrait;
 	
-	class DplusoURLS extends WireData {
-		protected $_self;
-		protected $path;
-		protected $urls = array(
-			'actions'  => array('_self' => 'activity'),
+	class DplusoURLConfig {
+		use ThrowErrorTrait;
+		
+		/**
+		 * Root Path to build off from
+		 * @var string
+		 */
+		static $_rootpath;
+		
+		/**
+		 * Domain or IP address to send Requests to 
+		 * @var string
+		 */
+		static $_serveraddress;
+		
+		/**
+		 * Root Directory for Server
+		 * NOTE Mainly used if IP address is used
+		 * @var string
+		 */
+		static $_serverdirectory;
+		
+		/**
+		 * Base Path, is built off self::$_rootpath
+		 * @var string
+		 */
+		protected $_path;
+		
+		/**
+		 * Constructor Function
+		 * Sets the basepath from the static property
+		 */
+		function __construct() {
+			$this->_path = self::$_rootpath;
+			$this->urls = new DplusoURLS();
+		}
+		
+		/**
+		 * Returns root path
+		 * @return string Root Path
+		 */
+		function __toString() {
+			return $this->_path;
+		}
+		
+		/**
+		 * Returns the path that is found for the given $key
+		 * @param  string $key Path Key 
+		 * @return string      URL Path
+		 */
+		function __get($key) {
+			$path = $this->urls->get_pathfromkey($key);
+			return "{$this->_path}$path/";
+		}
+		
+		
+		static function set_rootpath($path) {
+			self::$_rootpath = $path;
+		}
+	}
+	
+	class DplusoURLS implements \ArrayAccess {
+		/**
+		 * URL Paths
+		 * NOTE if an element is an array then that could mean the element is a menu with subelements making references to subpages of that menu
+		 * the '_self' value is the value for the key it traveled from
+		 * so $urls['activity']['_self'] would point to where activity page is supposed to be
+		 * @var array
+		 */
+		
+		private $urls = array(
+			'actions'  => 'activity',
 			'activity' => array(
 				'_self'       => 'activity',
 				'useractions' => 'user-actions'
@@ -81,40 +148,89 @@
 			),
 		);
 		
-		function __construct($path) {
-			$this->data = new WireData();
-			$this->_self = $path;
-		}
+		public function offsetSet($offset, $value) {
+	        if (is_null($offset)) {
+	            $this->urls[] = $value;
+	        } else {
+	            $this->urls[$offset] = $value;
+	        }
+	    }
+
+	    public function offsetExists($offset) {
+	        return isset($this->urls[$offset]);
+	    }
+
+	    public function offsetUnset($offset) {
+	        unset($this->urls[$offset]);
+	    }
+
+	    public function offsetGet($offset) {
+	        return isset($this->urls[$offset]) ? $this->urls[$offset] : null;
+	    }
 		
-		function __toString() {
-			return $this->_self;
-		}
-		
-		function __get($key) {
+		/**
+		 * Returns the paths found by parsing the key into an array 
+		 * 1. $key is parsed into an array delimited by an underscore (_)
+		 *      Example $key = "vendor"       -> $keys = array('vendor');
+		 *      Example $key = "vendor_info"  -> $keys = array('vendor', 'info')
+		 * 2.   Then we iterate through the $keys and go down the $this->urls array
+		 * @param  string $key Key to Page
+		 * @return string      Page Path
+		 */
+		protected function get_pathfromkey(string $key) {
 			$keys = explode('_', $key);
-			$path = $this->get_pathfromarray($this->urls, $keys);
-			return "{$this->_self}$path/";
+			$paths = array();
+			
+			// Check that this key exists else throw error
+			if (isset($this->urls[$keys[0]])) {
+				
+				// Check if this $keys[0] is an array, then travel down
+				if (is_array($this->urls[$keys[0]])) {
+					
+					// Set array to the array value found at $keys[0]
+					$level = $this->urls[$keys[0]];
+					
+					// Automatically append the level's _self value to $paths, then remove it from $keys
+					$paths[] = $level['_self'];
+					array_shift($keys);
+					
+					while (is_array($level) && !empty($keys)) {
+						
+						// Check that this key exists else throw error
+						if (key_exists($keys[0], $level)) {
+							if (is_array($level[$keys[0]])) {
+								// Add $levels[$key[0]]['_self'] to Paths
+								// Set $level to $levels[$key[0]] to travel down to
+								$paths[] = array($level[$keys[0]]['_self']);
+								$level = $level[$keys[0]];
+								array_shift($keys);
+							} else {
+								// $level[$keys[0]] is a value so we just append that value to paths
+								$paths[] = $level[$keys[0]];
+								break;
+							}
+						} else {
+							$this->error_keypath($keys, $paths);
+						}
+					}
+					return implode('/', $paths);
+				} else {
+					return $this->urls[$keys[0]];
+				}
+			} else {
+				$this->error_keypath($keys, $paths);
+			}
 		}
 		
-		protected function get_pathfromarray($urlarray, $keys) {
-			if (is_array($urlarray[$keys[0]])) {
-				$array = $urlarray[$keys[0]];
-				$paths = array($urlarray[$keys[0]]['_self']);
-				array_shift($keys);
-				
-				while (is_array($array) && !empty($keys)) {
-					if (is_array($array[$keys[0]])) {
-						$paths[] = array($array[$keys[0]]['_self']);
-						$array = $array[$keys[0]];
-						array_shift($keys);
-					} else {
-						$paths[] = $array[$keys[0]];
-						break;
-					}
-				}
-				return implode('/', $paths);
-			} else {
-				return $urlarray[$keys[0]];
-			}
+		/**
+		 * Throws an E_USER_ERROR because Key was not found in array
+		 * @param  array  $keys  Keys Array
+		 * @param  array  $paths Paths array
+		 * @return void
+		 */
+		protected function error_keypath(array $keys, array $paths) {
+			$currentkey = $keys[0];
+			$path = implode('/', $paths);
+			$this->error("$currentkey was not found under $path");
 		}
 	}
