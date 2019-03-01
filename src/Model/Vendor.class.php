926feabaa437abc7908f8db356f051fe480fba8a
Paul Gomez <pauldro952@outlook.com>
2019-03-01 14:11:37 -0600

    Remove Vendor URL generation from model class

--- src/Model/Vendor.class.php
+++ src/Model/Vendor.class.php
@@ -1,107 +1,111 @@
 <?php
-	use Dplus\ProcessWire\DplusWire;
-	
-	use Purl\Url;
-	
+	/**
+	 * Internal Libraries
+	 */
+	use Dplus\Base\ThrowErrorTrait;
+	use Dplus\Base\MagicMethodTraits;
+	use Dplus\Base\CreateFromObjectArrayTraits;
+	use Dplus\Base\CreateClassArrayTraits;
+
 	/**
 	 * Deals with Vendors from vendors Table
 	 */
 	class Vendor {
-		use Dplus\Base\ThrowErrorTrait;
-		use Dplus\Base\MagicMethodTraits;
-		use Dplus\Base\CreateFromObjectArrayTraits;
-		use Dplus\Base\CreateClassArrayTraits;
-		
+		use ThrowErrorTrait;
+		use MagicMethodTraits;
+		use CreateFromObjectArrayTraits;
+		use CreateClassArrayTraits;
+
 		/**
 		 * Vendor ID
 		 * @var string
 		 */
 		protected $vendid;
-		
+
 		/**
 		 * Vendor Ship From
 		 * @var string
 		 */
 		protected $shipfrom;
-		
+
 		/**
 		 * Vendor Name
 		 * @var string
 		 */
 		protected $name;
-		
+
 		/**
 		 * Vendor Address 1
 		 * @var string
 		 */
 		protected $address1;
-		
+
 		/**
 		 * Vendor Address 2
 		 * @var string
 		 */
 		protected $address2;
-		
+
 		/**
 		 * Vendor Address 3
 		 * @var string
 		 */
 		protected $address3;
-		
+
 		/**
 		 * City
 		 * @var string
 		 */
 		protected $city;
-		
+
 		/**
 		 * State
 		 * @var string
 		 */
 		protected $state;
-		
+
 		/**
 		 * Zip Code
 		 * @var string
 		 */
 		protected $zip;
-		
+
 		/**
 		 * Country
 		 * @var string
 		 */
 		protected $country;
-		
+
 		/**
 		 * Phone Number
 		 * @var string
 		 */
 		protected $phone;
-		
+
 		/**
 		 * Fax Number
 		 * @var string
 		 */
 		protected $fax;
-		
+
 		/**
 		 * Email Address
 		 * @var string
 		 */
 		protected $email;
-		
+
 		/**
 		 * Updated Time
 		 * @var int HHMMSSSS
 		 */
 		protected $createtime;
-		
+
 		/**
 		 * Updated Date
 		 * @var int YYYYMMDD
 		 */
 		protected $createdate;
-		
+
 		/**
 		 * Property Aliases
 		 * @var array
@@ -138,22 +142,6 @@
 		 */
 		public function has_shipfrom() {
 			return (!empty($this->shipfrom));
-		}
-
-		/**
-		 * // TODO rename for URL()
-		 * Returns URL to load Vendor Information page for that Vendor
-		 * @param  bool   $withshipfrom Whether or not to use $this->shipfromid
-		 * @return string URL to load Vendor Information page
-		 */
-		public function generate_viurl($withshipfrom = true) {
-			$url = new Url(DplusWire::wire('config')->pages->vendorinfo);
-			$url->path->add($this->vendid);
-
-			if ($withshipfrom) {
-				$url->path->add('shipfrom-'.$this->shipfrom);
-			}
-			return $url->getUrl();
 		}
 
 		/* =============================================================
