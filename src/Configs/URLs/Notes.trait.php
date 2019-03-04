7b6256f5757747757029b9960a9d73ca4c5ece42
Paul Gomez <pauldro952@outlook.com>
2019-03-04 11:05:19 -0600

    updates: Notes

--- src/Configs/URLs/Notes.trait.php
+++ src/Configs/URLs/Notes.trait.php
@@ -11,12 +11,23 @@
 	 */
 	trait NoteURLsTrait {
 		/**
+		 * Returns URL PATH to the Notes Redir Page
+		 * @return string
+		 */
+		public function get_notes_redirURL() {
+			$url = new Url($this->paths->get_urlpath('notes'));
+			$url->path->add('redir');
+			return $url->getUrl();
+		}
+			
+
+		/**
 		 * Returns URL to request the Qnotes for a Cart Detail Line
 		 * @param  int    $linenbr
 		 * @return string          URL for Requesting Qnotes for a Cart Detail Line
 		 */
 		public function get_request_cart_dplusnotesURL(int $linenbr) {
-			$url = new Url($this->get_cart_redirURL());
+			$url = new Url($this->get_notes_redirURL());
 			$url->query->set('action', 'get-cart-notes');
 			$url->query->set('linenbr', $linenbr);
 			return $url->getUrl();
